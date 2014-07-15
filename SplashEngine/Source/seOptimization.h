10189decdf815eefb16884b3065e425f9f07d4d1
Kyle Finlay <contact.blackraininteractive@gmail.com>
2014-07-15 20:40:40 +1000

    New optimization header file.

---
+++ SplashEngine/Source/seOptimization.h
@@ -0,0 +1,9 @@
+//============================================================================
+// Name        : seOptimization.h
+// Author      : Kyle Finlay
+// Copyright   : 2014 by Black Rain Interactive
+// Description : This file is a part of Splash Engine.
+//				 It includes optimization classes.
+//============================================================================
+
+#include "Optimization/BatchGroup/BatchGroup.h"
